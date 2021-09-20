edc543cee1d935a353628cc97ee60c19507e9996
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-09-20 06:31:38 -0400

    fix: log organization

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -8,7 +8,7 @@ const notFound = (err, _req, res, next) => {
   const { message, code } = err;
   const date = new Date();
   const content = `${date} - ERROR - ${code} -> ${message}`;
-  writeFile(`./data/log[${date}].txt`, content)
+  writeFile(`./logs/${date}.txt`, content)
     .then(() => res.status(400).send(content))
     .catch((e) => next(e));
 }
