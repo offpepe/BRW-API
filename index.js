2cf07a3f93f5f24bcbf4621c1c3e5eeb216f8794
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-09-20 04:52:25 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3001
+
+app.get('/', (_req, res) => res.status(200).send('Welcome to BRW API V1'));
+
+app.listen(port, () => console.log(`BRW API is running on ${port}`));
