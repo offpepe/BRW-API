eac5c8c9da8206ab650d5d78988714aa543d9cbe
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-09-20 06:17:14 -0400

    feat: error middleware

---
+++ middlewares/errors.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const util = require('util');
+
+const writeFile = util.promisify(fs.writeFile);
+
+
+const notFound = (err, _req, res, next) => {
+  const { message, code } = err;
+  const date = Date.now();
+  const content = `${date} - ERROR - ${code} -> ${message}`;
+  writeFile(`./data/log[${date}].txt`, content)
+    .then(() => res.status(400).send(content))
+    .catch((e) => next(e));
+}
+
+module.exports = { notFound };
