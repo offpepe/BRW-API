24dd55ce9633775c1d581fd6b128d35a4f1f276b
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-09-20 05:30:04 -0400

    feat: BRW integrants data

---
+++ data/integrants.js
@@ -0,0 +1,102 @@
+const BRW = [
+    {
+        name: 'Alan Albuquerque',
+        nickname: ['Offpepe', 'Offplayer', 'Xunxunmaru'],
+    },
+    {
+        name: 'Jefferson Aivi Domingues',
+        nickname: ['Jeff', 'Mithy Pickle Rick'],
+    },
+    {
+        name: 'André Rodrigo',
+        nickname: ['Niggon']
+    },
+    {
+        name: 'Felipe Rezende',
+        nickname: ['Frze'],
+    },
+    {
+        name: 'Felipe Willyam',
+        nickname: ['Dumaul'],
+    },
+    {
+        name: 'Felipe Aivi Domingues',
+        nickname: ['Lipe Gordo'],
+    },
+    {
+        name: '?',
+        nickname: ['Bruxo'],
+    },
+    {
+        name: 'Abiel',
+        nickname: ['Abiel God'],
+    },
+    {
+        name: 'Nick Gutterres',
+        nickname: ['Nick'],
+    },
+    {
+        name: '?',
+        nickname: ['Jotapex'],
+    },
+    {
+        name: 'Lucas',
+        nickname: ['Otaco'],
+    },
+    {
+        name: 'Diego',
+        nickname: ['Dimba'],
+    },
+    {
+        name: 'Jorge',
+        nickname: ['Esteche.Jorge'],
+    },
+    {
+        name: 'Marcelo',
+        nickname: ['TinkTynk'],
+    },
+    {
+        name: 'Tetsuya',
+        nickname: ['Jinx', 'Tetsuya'],
+    },
+    {
+        name: 'Lucas Silva',
+        nickname: ['Lukzone'],
+    },
+    {
+        name: 'Denis Melo',
+        nickname: ['Donis', 'Mr Deidara'],
+    },
+    {
+        name: '?',
+        nickname: ['Marquin'],
+    }
+];
+
+const Brodi = [
+    {
+      name: 'Jaqueline',
+      nickname: ['jujubaklim'],
+    },
+    {
+      name: 'Giovanna Goulart',
+      nickname: ['Gio'],
+    },
+    {
+      name: 'Wesly Dias',
+      nickname: ['MrWeslee'],
+    },
+    {
+      name: 'Gustavo Camargo',
+      nickname: ['Megazord'],
+    },
+    {
+       name: 'David',
+       nickname: ['Powermatanza'], 
+    }
+]
+
+module.exports = {
+    BRW,
+    Brodi,
+};
--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const cors = require('cors');
 const app = express();
 const port = 3001
 
+app.use(cors());
+app.use(bodyParser.json());
+
 app.get('/', (_req, res) => res.status(200).send('Welcome to BRW API V1'));
 
 app.listen(port, () => console.log(`BRW API is running on ${port}`));
