0e3f10120b30955e6e3b2e293110b8b59e9b83d2
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-09-20 05:58:27 -0400

    feat: added routes on app and testing[works]

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const integrantsEndPoints = require('./routes/integrants');
 const app = express();
 const port = 3001
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (_req, res) => res.status(200).send('Welcome to BRW API V1'));
+app.use('/', integrantsEndPoints);
 
 app.listen(port, () => console.log(`BRW API is running on ${port}`));
